aefe7836b421e5879bd98fd775a493a99b7540b3
5gi <5gi.exist@gmail.com>
2023-06-06 17:33:21 -0400

    1.2.0

--- JavaLaunchCreatorMC/JavaLaunchCreatorMC/Form1.Designer.cs
+++ JavaLaunchCreatorMC/JavaLaunchCreatorMC/Form1.Designer.cs
@@ -1,4 +1,6 @@
-﻿namespace JavaLaunchCreatorMC
+﻿using System.Threading;
+
+namespace JavaLaunchCreatorMC
 {
     partial class Form1
     {
@@ -45,19 +47,26 @@
             this.button4 = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.label6 = new System.Windows.Forms.Label();
-            this.button2 = new System.Windows.Forms.Button();
             this.toggleuela = new System.Windows.Forms.Button();
             this.label7 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.label10 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button6 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // Logo
             // 
             this.Logo.AutoSize = true;
-            this.Logo.Font = new System.Drawing.Font("Minecraft", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Logo.Location = new System.Drawing.Point(3, 8);
+            this.Logo.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Logo.Location = new System.Drawing.Point(31, 9);
             this.Logo.Name = "Logo";
-            this.Logo.Size = new System.Drawing.Size(328, 23);
+            this.Logo.Size = new System.Drawing.Size(280, 24);
             this.Logo.TabIndex = 0;
             this.Logo.Text = "Minecraft Server Launch Creator";
             // 
@@ -74,13 +83,13 @@
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(8, 48);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(300, 13);
+            this.label1.Size = new System.Drawing.Size(80, 13);
             this.label1.TabIndex = 2;
-            this.label1.Text = "Window Name (Note: The .sh file doesn\'t support this feature):";
+            this.label1.Text = "Window Name:";
             // 
             // serverFile
             // 
-            this.serverFile.Location = new System.Drawing.Point(8, 120);
+            this.serverFile.Location = new System.Drawing.Point(8, 105);
             this.serverFile.Name = "serverFile";
             this.serverFile.ReadOnly = true;
             this.serverFile.Size = new System.Drawing.Size(296, 20);
@@ -89,7 +98,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(304, 119);
+            this.button1.Location = new System.Drawing.Point(304, 104);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(24, 22);
             this.button1.TabIndex = 4;
@@ -100,7 +109,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(8, 104);
+            this.label2.Location = new System.Drawing.Point(8, 89);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(60, 13);
             this.label2.TabIndex = 5;
@@ -108,7 +117,7 @@
             // 
             // togglegui
             // 
-            this.togglegui.Location = new System.Drawing.Point(56, 163);
+            this.togglegui.Location = new System.Drawing.Point(55, 144);
             this.togglegui.Name = "togglegui";
             this.togglegui.Size = new System.Drawing.Size(75, 23);
             this.togglegui.TabIndex = 6;
@@ -128,7 +137,7 @@
             // guilabel
             // 
             this.guilabel.AutoSize = true;
-            this.guilabel.Location = new System.Drawing.Point(67, 148);
+            this.guilabel.Location = new System.Drawing.Point(66, 129);
             this.guilabel.Name = "guilabel";
             this.guilabel.Size = new System.Drawing.Size(54, 13);
             this.guilabel.TabIndex = 8;
@@ -170,7 +179,7 @@
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(88, 192);
+            this.button4.Location = new System.Drawing.Point(88, 214);
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(152, 23);
             this.button4.TabIndex = 14;
@@ -180,35 +189,24 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(200, 165);
+            this.textBox1.Location = new System.Drawing.Point(212, 186);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(128, 20);
+            this.textBox1.Size = new System.Drawing.Size(116, 20);
             this.textBox1.TabIndex = 15;
             this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(205, 148);
+            this.label6.Location = new System.Drawing.Point(209, 170);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(119, 13);
             this.label6.TabIndex = 16;
             this.label6.Text = "Ram Allocated (ex: 2G):";
             // 
-            // button2
-            // 
-            this.button2.Enabled = false;
-            this.button2.Location = new System.Drawing.Point(88, 216);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(152, 23);
-            this.button2.TabIndex = 17;
-            this.button2.Text = "Run Script (Batch)";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
-            // 
             // toggleuela
             // 
-            this.toggleuela.Location = new System.Drawing.Point(136, 163);
+            this.toggleuela.Location = new System.Drawing.Point(135, 144);
             this.toggleuela.Name = "toggleuela";
             this.toggleuela.Size = new System.Drawing.Size(56, 23);
             this.toggleuela.TabIndex = 18;
@@ -219,20 +217,108 @@
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(136, 148);
+            this.label7.Location = new System.Drawing.Point(135, 129);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(55, 13);
             this.label7.TabIndex = 19;
             this.label7.Text = "eula: false";
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(55, 184);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(80, 22);
+            this.button2.TabIndex = 20;
+            this.button2.Text = "Toggle";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(52, 170);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(94, 13);
+            this.label8.TabIndex = 21;
+            this.label8.Text = "BungeeCord: false";
+            this.label8.Click += new System.EventHandler(this.label8_Click);
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(209, 129);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(115, 13);
+            this.label9.TabIndex = 22;
+            this.label9.Text = "Server Type: Windows";
+            // 
+            // button3
+            // 
+            this.button3.Enabled = false;
+            this.button3.Location = new System.Drawing.Point(201, 142);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(62, 23);
+            this.button3.TabIndex = 23;
+            this.button3.Text = "Windows";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(266, 142);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(62, 23);
+            this.button5.TabIndex = 24;
+            this.button5.Text = "Linux";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(157, 170);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(26, 13);
+            this.label10.TabIndex = 25;
+            this.label10.Text = "Port";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(141, 186);
+            this.textBox2.MaxLength = 5;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(49, 20);
+            this.textBox2.TabIndex = 26;
+            this.textBox2.Text = "25565";
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            this.textBox2.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox2_KeyPress);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(184, 186);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(22, 20);
+            this.button6.TabIndex = 27;
+            this.button6.Text = ".";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(335, 450);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.label7);
             this.Controls.Add(this.toggleuela);
-            this.Controls.Add(this.button2);
             this.Controls.Add(this.label6);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button4);
@@ -279,9 +365,16 @@
         private System.Windows.Forms.Button button4;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Label label6;
-        private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button toggleuela;
         private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button6;
     }
 }
 
--- JavaLaunchCreatorMC/JavaLaunchCreatorMC/Form1.cs
+++ JavaLaunchCreatorMC/JavaLaunchCreatorMC/Form1.cs
@@ -21,7 +21,9 @@ namespace JavaLaunchCreatorMC
         string filePath = string.Empty;
         string fileName = string.Empty;
         string ram = string.Empty;
+        string serverType = "Windows";
 
+        bool bungeeCord = false;
         bool gui = false;
         bool eula = false;
 
@@ -59,13 +61,6 @@ namespace JavaLaunchCreatorMC
             {
                 filePath = openFileDialog.FileName;
                 fileName = Path.GetFileNameWithoutExtension(openFileDialog.FileName) + ".jar";
-                if (File.Exists(filePath.Replace(fileName, "") + "start.bat"))
-                {
-                    button2.Enabled = true;
-                } else
-                {
-                    button2.Enabled = false;
-                }
             }
             updateServerFileText();
         }
@@ -83,27 +78,29 @@ namespace JavaLaunchCreatorMC
                 MessageBox.Show("Please Select a file to make a script for!", "Warning!", MessageBoxButtons.OK);
                 return;
             }
-
             if (ram.Equals(""))
             {
                 MessageBox.Show("Please fill out the \"Ram Allocated\" Field!", "Warning!", MessageBoxButtons.OK);
                 return;
             }
-
-            if (eula == true)
+            if (eula == true && !bungeeCord) 
             {
                 File.WriteAllText(filePath.Replace(fileName, "") + "eula.txt", "eula=true");
             }
-
-            
-
-
             if (gui == true)
             {
                 MessageBox.Show("It is recommended that you turn off GUI for a better experience.", "Warning!", MessageBoxButtons.OK);
             }
-
-
+            if (textBox2.Enabled)
+            {
+                if (bungeeCord) 
+                {
+                    File.WriteAllText(filePath.Replace(fileName, "") + "config.yml", "  host: 0.0.0.0:" + textBox2.Text) ;
+                } else
+                {
+                    File.WriteAllText(filePath.Replace(fileName, "") + "server.properties", "server-port=" + textBox2.Text);
+                }
+            }
             StringBuilder stringBuilder = new StringBuilder();
             FileInfo file = new FileInfo(filePath);
             DriveInfo drive = new DriveInfo(file.Directory.Root.FullName);
@@ -115,17 +112,9 @@ namespace JavaLaunchCreatorMC
             {
                 stringBuilder.Append(" nogui");
             }
-            File.WriteAllText(filePath.Replace(fileName, "") + "start.bat" , stringBuilder.ToString());
-            File.WriteAllText(filePath.Replace(fileName, "") + "start.sh", stringBuilder.ToString().Replace("@echo off\n" + drive.ToString().Replace("\\", "") + "\n" + "title " + windowName + "\n", ""));
+            if (serverType.Equals("Windows")) File.WriteAllText(filePath.Replace(fileName, "") + "start.cmd" , stringBuilder.ToString());
+            if (serverType.Equals("Linux")) File.WriteAllText(filePath.Replace(fileName, "") + "start.sh", stringBuilder.ToString().Replace("@echo off\n" + drive.ToString().Replace("\\", "") + "\n" + "title " + windowName + "\n", ""));
             MessageBox.Show("Successfully generated the files!", "Finished" , MessageBoxButtons.OK);
-            if (File.Exists(filePath.Replace(fileName, "") + "start.bat"))
-            {
-                button2.Enabled = true;
-            }
-            else
-            {
-                button2.Enabled = false;
-            }
         }
 
         private void textBox1_TextChanged(object sender, EventArgs e)
@@ -136,27 +125,6 @@ namespace JavaLaunchCreatorMC
         private void serverFile_TextChanged(object sender, EventArgs e)
         {
 
-        }
-
-        private void button2_Click(object sender, EventArgs e)
-        {
-            if (File.Exists(filePath.Replace(fileName, "") + "start.bat"))
-            {
-                System.Diagnostics.Process.Start("cmd.exe", "/c " + filePath.Replace(fileName, "") + "start.bat");
-
-                /*Process cmd = new Process();
-                cmd.StartInfo.FileName = "cmd.exe";
-                *//*cmd.StartInfo.RedirectStandardInput = true;
-                cmd.StartInfo.RedirectStandardOutput = true;*//*
-                cmd.StartInfo.CreateNoWindow = false;
-                cmd.StartInfo.UseShellExecute = false;
-                cmd.Start();
-
-                cmd.StandardInput.WriteLine(filePath.Replace(fileName, "") + "start.bat");
-                cmd.StandardInput.Flush();
-                cmd.StandardInput.Close();
-                cmd.WaitForExit();*/
-            }
         }
 
         public void updateEULALabel()
@@ -171,6 +139,87 @@ namespace JavaLaunchCreatorMC
             }
             eula = !eula;
             updateEULALabel();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            bungeeCord = !bungeeCord;
+            if (bungeeCord)
+            {
+                toggleuela.Enabled = false;
+                eula = false;
+                updateEULALabel();
+            }
+            else
+            {
+                toggleuela.Enabled = true;
+            }
+            label8.Text = "BungeeCord: " + bungeeCord;
+        }
+
+        private void label8_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.serverType = "Windows";
+            this.label9.Text = "Server Type: " + serverType;
+            this.button3.Enabled = false;
+            this.button5.Enabled = true;
+            this.windowname.Enabled = true;
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            this.serverType = "Linux";
+            this.label9.Text = "Server Type: " + serverType;
+            this.button3.Enabled = true;
+            this.button5.Enabled = false;
+            this.windowname.Enabled = false;
+        }
+
+        private void textBox2_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar) &&
+                (e.KeyChar != '.'))
+            {
+                e.Handled = true;
+            }
+
+            // only allow one decimal point
+            if ((e.KeyChar == '.') && ((sender as TextBox).Text.IndexOf('.') > -1))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                int por = int.Parse(textBox2.Text);
+                if (por > 25565) textBox2.Text = "65535";
+            } catch (Exception)
+            {
+                //ignore
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            bungeeCord = !bungeeCord;
+            if (bungeeCord)
+            {
+                textBox2.Text = "disabled";
+                textBox2.Enabled = false;
+            }
+            else
+            {
+                textBox2.Text = "25565";
+                textBox2.Enabled = true;
+            }
         }
     }
 }
